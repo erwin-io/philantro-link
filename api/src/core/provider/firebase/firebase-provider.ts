e5125980ece8d7367d128b30123fae8145565bd0
erwin-io <erwinramirez220@gmail.com>
2024-09-04 00:56:11 +0800

    updates

--- api/src/core/provider/firebase/firebase-provider.ts
+++ api/src/core/provider/firebase/firebase-provider.ts
@@ -1,13 +1,27 @@
 import { Injectable } from "@nestjs/common";
 import * as admin from "firebase-admin";
-import firebaseConfig from "./firebase.config";
 import { ConfigService } from "@nestjs/config";
-import { getAuth } from "@firebase/auth";
 
 @Injectable()
 export class FirebaseProvider {
   public app: admin.app.App;
   constructor(private readonly config: ConfigService) {
+    const firebaseConfig = {
+      type: this.config.get<string>("FIREBASE_TYPE"),
+      projectId: this.config.get<string>("FIREBASE_PROJECT_ID"),
+      privateKeyId: this.config.get<string>("FIREBASE_PRIVATE_KEY_ID"),
+      privateKey: this.config.get<string>("FIREBASE_PRIVATE_KEY"),
+      clientEmail: this.config.get<string>("FIREBASE_CLIENT_EMAIL"),
+      clientId: this.config.get<string>("FIREBASE_CLIENT_ID"),
+      authUri: this.config.get<string>("FIREBASE_AUTH_URI"),
+      tokenUri: this.config.get<string>("FIREBASE_TOKEN_URI"),
+      authProviderX509CertUrl: this.config.get<string>(
+        "FIREBASE_AUTH_PROVIDER_X509_CERT_URL"
+      ),
+      clientC509CertUrl: this.config.get<string>(
+        "FIREBASE_CLIENT_X509_CERT_URL"
+      ),
+    };
     this.app = admin.initializeApp({
       credential: admin.credential.cert(firebaseConfig),
       storageBucket: this.config.get<string>("FIREBASE_BUCKET"),
